bd9876bbedc33e4a2c3902fe0bb3a42806bfefa9
Daniel Arroyo <danielh989@gmail.com>
2020-12-28 22:12:44 -0300

    ADD: Style propagation

--- app/components/AppText.js
+++ app/components/AppText.js
@@ -1,10 +1,19 @@
 import React from "react";
-import { StyleSheet, Text } from "react-native";
+import { Platform, StyleSheet, Text } from "react-native";
 function AppText({ children }) {
   return <Text style={styles.text}>{children}</Text>;
 }
 
 const styles = StyleSheet.create({
   text: { fontSize: 14 },
+  ...Platform.select({
+    ios: {
+      fontSize: 20,
+      fontFamily: "Avenir",
+    },
+    android: {
+      fontSize: 18,
+    },
+  }),
 });
 export default AppText;
