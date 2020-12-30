4fc4c2f7b319842af4d00f2ff00fd312a0d988a1
Daniel Arroyo <danielh989@gmail.com>
2020-12-30 15:27:46 -0300

    CHG: List item extracted

---
+++ app/components/ListItem.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { View, StyleSheet, Image } from "react-native";
+
+import { fontSizes, colors } from "../config/params";
+import AppText from "./AppText";
+
+function ListItem({ title, subtitle, image }) {
+  return (
+    <View style={styles.container}>
+      <Image style={styles.image} source={image}></Image>
+      <View style={styles.textContainer}>
+        <AppText style={styles.title}>{title}</AppText>
+        <AppText style={styles.subtitle}>{subtitle}</AppText>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { margin: 20, flexDirection: "row" },
+  image: { height: 60, width: 60, borderRadius: 30 },
+  textContainer: { marginLeft: 15 },
+  title: { fontSize: fontSizes.medium },
+  subtitle: { color: colors.grey },
+});
+
+export default ListItem;
--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { Image, View, StyleSheet } from "react-native";
-import AppText from "../components/AppText";
 
+import AppText from "../components/AppText";
+import ListItem from "../components/ListItem";
 import { colors, fontSizes } from "../config/params";
 
 function ListingDetailsScreen(props) {
@@ -11,27 +12,22 @@ function ListingDetailsScreen(props) {
         style={styles.image}
         source={require("../assets/jacket.jpg")}
       ></Image>
-      <View style={styles.detailTexts}>
+      <View style={styles.detailContainer}>
         <AppText style={styles.title}>Green jacket for sale!</AppText>
         <AppText style={styles.price}>$100</AppText>
       </View>
-      <View style={styles.uploader}>
-        <Image
-          style={styles.avatar}
-          source={require("../assets/user.jpg")}
-        ></Image>
-        <View style={styles.userTexts}>
-          <AppText style={styles.name}>Danielle</AppText>
-          <AppText style={styles.listings}>5 Listings</AppText>
-        </View>
-      </View>
+      <ListItem
+        image={require("../assets/user.jpg")}
+        title="Danielle"
+        subtitle="5 Listings"
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   image: { width: "100%", height: 300 },
-  detailTexts: { margin: 15, marginLeft: 20 },
+  detailContainer: { margin: 15, marginLeft: 20 },
   title: { fontSize: fontSizes.large },
   price: {
     color: colors.secondary,
@@ -39,11 +35,6 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     marginTop: 6,
   },
-  uploader: { margin: 20, flexDirection: "row" },
-  avatar: { height: 60, width: 60, borderRadius: 30 },
-  userTexts: { marginLeft: 15 },
-  name: { fontSize: fontSizes.medium },
-  listings: { color: colors.grey },
 });
 
 export default ListingDetailsScreen;
