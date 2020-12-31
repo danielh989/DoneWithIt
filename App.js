0a1f62f41f2b5509e1ef0df8bad7f10e9aee8248
Daniel Arroyo <danielh989@gmail.com>
2020-12-31 12:46:46 -0300

    ADD: Listings screen

--- App.js
+++ App.js
@@ -1,14 +1,10 @@
 import React from "react";
 import { StyleSheet } from "react-native";
 
-import AccountScreen from "./app/screens/AccountScreen";
+import ListingsScreen from "./app/screens/ListingsScreen";
 
 export default function App() {
-  return <AccountScreen></AccountScreen>;
+  return <ListingsScreen></ListingsScreen>;
 }
 
-const styles = StyleSheet.create({
-  card: {
-    backgroundColor: "whitesmoke",
-  },
-});
+const styles = StyleSheet.create({});
---
+++ app/screens/ListingsScreen.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { FlatList } from "react-native-gesture-handler";
+
+import Card from "../components/Card";
+import Screen from "../components/Screen";
+
+function ListingsScreen(props) {
+  const items = [
+    {
+      title: "Listing 1",
+      subtitle: "Subtitle 1",
+      image: require("../assets/jacket.jpg"),
+    },
+    {
+      title: "Listing 2",
+      subtitle: "Subtitle 2",
+      image: require("../assets/jacket.jpg"),
+    },
+  ];
+  return (
+    <Screen>
+      <FlatList
+        data={items}
+        keyExtractor={(item) => item.title}
+        renderItem={({ item }) => (
+          <Card
+            title={item.title}
+            subtitle={item.subtitle}
+            image={item.image}
+          />
+        )}
+      />
+    </Screen>
+  );
+}
+
+export default ListingsScreen;
