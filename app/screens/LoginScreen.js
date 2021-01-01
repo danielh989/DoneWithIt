ebddc897c318be063df1110b1abde3bb67aa80cd
Daniel Arroyo <danielh989@gmail.com>
2021-01-01 13:43:40 -0300

    ADD: Validations with Yup

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,21 +1,28 @@
 import React from "react";
-
 import { Formik } from "formik";
+import * as Yup from "yup";
 
 import Screen from "../components/Screen";
 import AppTextInput from "../components/AppTextInput";
+import AppText from "../components/AppText";
 import AppButton from "../components/AppButton";
 
 function LoginScreen(props) {
+  const validationSchema = Yup.object().shape({
+    email: Yup.string().required().email().label("Email"),
+    password: Yup.string().required().min(4).label("Password"),
+  });
   return (
     <Screen>
       <Formik
         initialValues={{ email: "", password: "" }}
         onSubmit={(values) => console.log({ values })}
+        validationSchema={validationSchema}
       >
-        {({ handleChange, handleSubmit }) => {
+        {({ handleChange, handleSubmit, errors }) => {
           return (
             <>
+              <AppText style={{ color: "red" }}>{errors.email}</AppText>
               <AppTextInput
                 autoCapitalize="none"
                 autoCorrect={false}
@@ -26,6 +33,7 @@ function LoginScreen(props) {
                 placeholder="Email"
                 textContentType="emailAddress"
               />
+              <AppText style={{ color: "red" }}>{errors.password}</AppText>
               <AppTextInput
                 autoCapitalize="none"
                 autoCorrect={false}
