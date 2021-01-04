3dada985bdfe9b8121630cff7a1f110dff8b039d
Daniel Arroyo <danielh989@gmail.com>
2021-01-04 11:20:24 -0300

    ADD: Trying image load

--- App.js
+++ App.js
@@ -3,10 +3,25 @@ import { StyleSheet, Button, Image } from "react-native";
 import * as ImagePicker from "expo-image-picker";
 
 import Screen from "./app/components/Screen";
-import ImageInput from "./app/components/ImageInput";
 
 export default function App() {
-  return <ImageInput></ImageInput>;
+  const [imageUri, setImageUri] = useState();
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      if (!result.cancelled) {
+        setImageUri(result.uri);
+      }
+    } catch (error) {
+      console.log("There was an error trying to load the image", error);
+    }
+  };
+  return (
+    <Screen>
+      <Button title="Select Image" onPress={selectImage}></Button>
+      <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
+    </Screen>
+  );
 }
 
 const styles = StyleSheet.create({});
--- app/components/ImageInput.js
+++ app/components/ImageInput.js
@@ -1,45 +1,17 @@
-import React, { useState } from "react";
-import { StyleSheet, View, Button } from "react-native";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import * as ImagePicker from "expo-image-picker";
-
-import ImageInputItem from "./ImageInputItem";
-import { FlatList } from "react-native-gesture-handler";
-
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import params from "../config/params";
 function ImageInput(props) {
-  const [images, setImages] = useState([]);
-  const requestImagePermission = async () => {
-    try {
-      const result = await ImagePicker.requestMediaLibraryPermissionsAsync();
-
-      if (result.granted) {
-        const image = await ImagePicker.launchImageLibraryAsync();
-        images.push({ uri: image.uri });
-        setImages(images);
-      }
-    } catch (error) {
-      alert("There was an error requesting the image.");
-    }
-  };
-
-  return (
-    <View style={styles.container}>
-      <FlatList
-        data={images}
-        keyExtractor={(item) => item.uri}
-        renderItem={(item) => {
-          console.log(item.uri);
-          <ImageInputItem uri={item.uri}></ImageInputItem>;
-        }}
-      ></FlatList>
-      <ImageInputItem
-        iconName="camera"
-        onPress={requestImagePermission}
-      ></ImageInputItem>
-    </View>
-  );
+  return <View style={styles.container}></View>;
 }
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    alignItems: "center",
+    backgroundColor: params.colors.lightgrey,
+    borderRadius: 15,
+    justifyContent: "center",
+    height: 100,
+    width: 100,
+  },
 });
 export default ImageInput;
