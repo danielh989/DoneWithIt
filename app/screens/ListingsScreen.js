ab4e20e345f6a5e6fa7648e0c9ebeaf252d4e0a1
Daniel Arroyo <danielh989@gmail.com>
2021-01-07 10:57:40 -0300

    ADD: Lottie animation

--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { FlatList } from "react-native";
+import { ActivityIndicator, FlatList } from "react-native";
 
 import Card from "../components/Card";
 import Screen from "../components/Screen";
@@ -7,30 +7,40 @@ import routes from "../navigation/routes";
 import listingsApi from "../api/listings";
 import AppText from "../components/AppText";
 import AppButton from "../components/AppButton";
+import params from "../config/params";
 
 function ListingsScreen({ navigation }) {
   const [listings, setListings] = useState([]);
   const [error, setError] = useState();
+  const [loading, setLoading] = useState();
 
   useEffect(() => {
     loadListings();
   }, []);
 
   const loadListings = async () => {
+    setLoading(true);
     const response = await listingsApi.getListings();
     if (!response.ok) return setError(true);
+    setLoading(false);
     setError(false);
     setListings(response.data);
   };
 
   return (
     <Screen>
+      <ActivityIndicator
+        animating={true}
+        size={"large"}
+        color={params.colors.lightgrey}
+      />
       {error && (
         <>
           <AppText>Couldn's retrieve the listings.</AppText>
           <AppButton title="Retry" onPress={loadListings}></AppButton>
         </>
       )}
+
       <FlatList
         data={listings}
         keyExtractor={(item) => item.title}
