234316b2f65b56d4511ccc55ce205346a5afec31
Daniel Arroyo <danielh989@gmail.com>
2021-01-07 17:09:37 -0300

    ADD: Testing netInfo event

--- App.js
+++ App.js
@@ -1,17 +1,24 @@
 import React from "react";
 import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
+import NetInfo from "@react-native-community/netinfo";
 
 import navigationTheme from "./app/navigation/navigationTheme";
 import AppNavigator from "./app/navigation/AppNavigator";
 import listings from "./app/api/listings";
 
 export default function App() {
-  return (
-    <NavigationContainer theme={navigationTheme}>
-      <AppNavigator />
-    </NavigationContainer>
+  //componentDidMount
+  const unsubscribe = NetInfo.addEventListener((netInfo) =>
+    console.log(netInfo)
   );
+  //componentWillUnmount
+  unsubscribe();
+
+  return null;
+  /*  { <NavigationContainer theme={navigationTheme}>
+      <AppNavigator />
+    </NavigationContainer> } */
 }
 
 const styles = StyleSheet.create({});
