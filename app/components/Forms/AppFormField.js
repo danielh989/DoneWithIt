88a645980a6c029541a77f081a1c5396f8411984
Daniel Arroyo <danielh989@gmail.com>
2021-01-07 15:45:08 -0300

    FIX: handleChange

--- app/components/Forms/AppFormField.js
+++ app/components/Forms/AppFormField.js
@@ -5,13 +5,20 @@ import ErrorMessage from "./ErrorMessage";
 import AppTextInput from "../AppTextInput";
 
 function AppFormField({ name, width, ...otherProps }) {
-  const { setFieldTouched, handleChange, errors, touched } = useFormikContext();
+  const {
+    errors,
+    setFieldTouched,
+    setFieldValue,
+    touched,
+    values,
+  } = useFormikContext();
   return (
     <>
       <AppTextInput
         width={width}
         onBlur={() => setFieldTouched(name)}
-        onChangeText={handleChange(name)}
+        onChangeText={(text) => setFieldValue(name, text)}
+        value={values[name]}
         {...otherProps}
       />
       <ErrorMessage error={errors[name]} visible={touched[name]} />
