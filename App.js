82dc77cff8b4ea0c668bdc75c311d6c2948bc188
Daniel Arroyo <danielh989@gmail.com>
2021-01-07 17:46:41 -0300

    ADD: Testing asyncStorage

--- App.js
+++ App.js
@@ -2,15 +2,25 @@ import React from "react";
 import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import NetInfo, { useNetInfo } from "@react-native-community/netinfo";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import navigationTheme from "./app/navigation/navigationTheme";
 import AppNavigator from "./app/navigation/AppNavigator";
 import listings from "./app/api/listings";
 
 export default function App() {
-  const netInfo = useNetInfo();
-  console.log(netInfo.isInternetReachable);
+  const demo = async () => {
+    try {
+      await AsyncStorage.setItem("person", JSON.stringify({ id: 1 }));
+      const value = await AsyncStorage.getItem("person");
+      const person = JSON.parse(value);
+      console.log(person);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
+  demo();
   return null;
   /*  { <NavigationContainer theme={navigationTheme}>
       <AppNavigator />
