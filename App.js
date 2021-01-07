6e884b72e8b818cb821d77bd4216d4a34f7fc264
Daniel Arroyo <danielh989@gmail.com>
2021-01-07 17:11:35 -0300

    FIX: Using hook for netInfo instead

--- App.js
+++ App.js
@@ -1,19 +1,15 @@
 import React from "react";
 import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import NetInfo from "@react-native-community/netinfo";
+import NetInfo, { useNetInfo } from "@react-native-community/netinfo";
 
 import navigationTheme from "./app/navigation/navigationTheme";
 import AppNavigator from "./app/navigation/AppNavigator";
 import listings from "./app/api/listings";
 
 export default function App() {
-  //componentDidMount
-  const unsubscribe = NetInfo.addEventListener((netInfo) =>
-    console.log(netInfo)
-  );
-  //componentWillUnmount
-  unsubscribe();
+  const netInfo = useNetInfo();
+  console.log(netInfo.isInternetReachable);
 
   return null;
   /*  { <NavigationContainer theme={navigationTheme}>
